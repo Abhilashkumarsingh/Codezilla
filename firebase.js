4bf034a50baef5c842ee8b4edecfe606db6da43e
Abhilashkumarsingh <43114987+Abhilashkumarsingh@users.noreply.github.com>
2020-06-30 16:03:30 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,16 @@
+var app_firebase={};
+(function(){
+	var firebaseConfig = {
+    apiKey: "AIzaSyBf8e8h8fZOx6WwFaXCt_v9a35C474USBY",
+    authDomain: "profile-2b9b6.firebaseapp.com",
+    databaseURL: "https://profile-2b9b6.firebaseio.com",
+    projectId: "profile-2b9b6",
+    storageBucket: "profile-2b9b6.appspot.com",
+    messagingSenderId: "715236646077",
+    appId: "1:715236646077:web:5e693884dbaafc5ad0abd4"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  app_firebase=firebase;
+})()
+  
